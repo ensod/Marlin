f7dd8f1498eab59656fbf2f3691f8a90e516cf80
ensod <esodupe@gmail.com>
2018-05-16 17:24:37 +0200

    Changed CONFIG_H_AUTHOR

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -74,7 +74,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-#define STRING_CONFIG_H_AUTHOR "(ESodupe, Witbox2+RAMPS1.4+Peltier, autobed level, 16/05/2017)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(EnriqueS, Marlin v1.1.0-RC7, Witbox2 with Rumba, v4)" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
